2d8a95f164f9677c4387c210c2ded386a370957e
juan-rod <juan.j.rodriguez2@gmail.com>
2019-10-08 19:10:52 -0500

    added stuff

---
+++ src/animate-tut/animations.js
@@ -0,0 +1,10 @@
+
+
+
+function openNav() {
+  // this is where you do stuff
+  // show me nav-item--mobile
+  let navMobile = document.querySelector('.nav-items--mobile')
+  navMobile.classList.toggle('showNav')
+  // console.log('navMobile:', navMobile.classList)
+}
